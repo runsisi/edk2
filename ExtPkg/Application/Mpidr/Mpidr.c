a6a32b9acb431ea35782a1297fe6bb35dbc30f6e
runsisi <runsisi@hust.edu.cn>
2022-02-17 21:54:18 +0800

    print mpidr
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

---
+++ ExtPkg/Application/Mpidr/Mpidr.c
@@ -0,0 +1,34 @@
+/** @file
+  This sample application bases on HelloWorld PCD setting
+  to print "UEFI Hello World!" to the UEFI Console.
+
+  Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/ArmLib.h>
+#include <Library/UefiLib.h>
+
+/**
+  The user Entry Point for Application. The user code starts with this function
+  as the real entry point for the application.
+
+  @param[in] ImageHandle    The firmware allocated handle for the EFI image.
+  @param[in] SystemTable    A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+  @retval other             Some error occurs when executing this entry point.
+
+**/
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+    UINT64 mpidr = ArmReadMpidr();
+    Print(L"Mpidr: 0x%x\n", mpidr);
+    return EFI_SUCCESS;
+}
